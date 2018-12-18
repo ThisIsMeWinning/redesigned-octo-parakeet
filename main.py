b055f1bcbd061ec91f7e9b437657ca3bad26b28d
ThisIsMeWinning <daltoncclinton@gmail.com>
2018-12-18 15:42:34 -0600

    game

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print ("Hello")
+import pygame
 
-print("test")
+background_color = (255, 255, 255)
+(width, height) = (900, 600)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Charooga')
+screen.fill(background_color)
+
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
