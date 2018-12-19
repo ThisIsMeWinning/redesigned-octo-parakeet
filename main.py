b24234e89e10d9119236b566fb193fdf8b716493
ThisIsMeWinning <daltoncclinton@gmail.com>
2018-12-19 09:02:13 -0600

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: ThisIsMeWinning <daltoncclinton@gmail.com>
    
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame
 import sys
+import random
 from pygame.locals import *
+
+TILESIZE = 32
+MAPWIDTH = 35
+MAPHEIGHT = 25
 
 # colors
 BLACK = (0, 0, 0)
@@ -13,6 +18,8 @@ GRASS = 1
 WATER = 2
 COAL = 3
 
+resources = [DIRT, GRASS, WATER, COAL]
+
 # dictionary for textures
 textures = {
     DIRT: pygame.image.load('resources\dirt.png'),
@@ -21,22 +28,25 @@ textures = {
     COAL: pygame.image.load('resources\coal.png')
 }
 
-tilemap = [
-    [GRASS, GRASS, GRASS, GRASS, GRASS],
-    [GRASS, WATER, WATER, WATER, GRASS],
-    [GRASS, DIRT, WATER, WATER, GRASS],
-    [GRASS, DIRT, COAL, GRASS, GRASS],
-    [GRASS, GRASS, GRASS, GRASS, GRASS]
-]
-
-TILESIZE = 32
-MAPWIDTH = 5
-MAPHEIGHT = 5
+tilemap = [[DIRT for w in range(MAPWIDTH)] for h in range(MAPHEIGHT)]
 
 pygame.init()
 
 DISPLAY = pygame.display.set_mode((MAPWIDTH*TILESIZE, MAPHEIGHT*TILESIZE))
 pygame.display.set_caption('2D Game')
+
+for rw in range(MAPHEIGHT):
+    for cl in range(MAPWIDTH):
+        randomNumber = random.randint(0,15)
+        if randomNumber == 0:
+            tile = COAL
+        elif randomNumber == 1 or randomNumber == 2:
+            tile = WATER
+        elif randomNumber >= 3 and randomNumber <= 7:
+            tile = GRASS
+        else:
+            tile = DIRT
+        tilemap[rw][cl] = tile
 
 while True:
     for event in pygame.event.get():
